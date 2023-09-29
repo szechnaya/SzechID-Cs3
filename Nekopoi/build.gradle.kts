8e2d8c73977ea717fd47a964fe6b38bf2997e6d6
szechnaya <redmoeid@gmail.com>
2023-09-29 20:55:27 +0700

    Start Build

--- Nekopoi/build.gradle.kts
+++ Nekopoi/build.gradle.kts
@@ -6,7 +6,7 @@ cloudstream {
     language = "id"
     // All of these properties are optional, you can safely remove them
 
-    // description = "Lorem Ipsum"
+     description = "Sangat Yabai :v"
      authors = listOf("Szechnaya")
 
     /**
